1dc8815c228cf81e320120368935cf423965067f
root <root@DESKTOP-I3723K1.localdomain>
2016-09-12 16:37:18 +0800

    update

---
+++ public/markdown.cpp
@@ -0,0 +1,16 @@
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
+// // mdtransform.hpp // // 保存目录的结构 typedef struct Cnode { vector <Cnode *> ch; string heading; string tag; Cnode (const string &hd): heading(hd) {} } Cnode; // 保存正文内容的结构 typedef struct node { int type; // 节点代表的类型 vector <node *> ch; string elem[3]; // 用来存放三个重要的属性, elem[0] 保存了要显示的内容 // elem[1] 保存了链接, elem[2] 则保存了 title node (int _type): type(_type) {} } node;
+
